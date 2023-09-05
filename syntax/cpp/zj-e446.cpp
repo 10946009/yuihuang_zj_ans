897d53d97563ef546d3ee571837d76b8badb26cb
10946009 <108606810+10946009@users.noreply.github.com>
2023-09-06 00:40:19 +0800

    Update zj-e446.cpp

--- syntax/cpp/zj-e446.cpp
+++ syntax/cpp/zj-e446.cpp
@@ -26,31 +26,3 @@ int main() {
     solve(1);
     return 0;
 }
-
-#include <iostream>
-using namespace std;
-int N, a[15];
-bool used[15];
-
-void solve(int d) {
-    if (d > N) {
-        for (int i = 1; i <= N; i++) {
-            cout << a[i] << " ";
-        }
-        cout << "\n";
-        return;
-    }
-    for (int i = 1; i <= N; i++) {
-        if (used[i]) continue;
-        used[i] = true;
-        a[d] = i;
-        solve(d + 1);
-        used[i] = false;
-    }
-}
-
-int main() {
-    cin >> N;
-    solve(1);
-    return 0;
-}
