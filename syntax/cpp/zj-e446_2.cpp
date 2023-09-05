ed15ac186a31085900b3f608fc28251395744bae
10946009 <108606810+10946009@users.noreply.github.com>
2023-09-06 00:40:33 +0800

    Update zj-e446_2.cpp

--- syntax/cpp/zj-e446_2.cpp
+++ syntax/cpp/zj-e446_2.cpp
@@ -18,23 +18,3 @@ int main() {
     } while (next_permutation(a, a+N));
     return 0;
 }
-
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-int main() {
-    int N;
-    cin >> N;
-    int a[N];
-    for (int i = 1; i <= N; i++) {
-        a[i-1] = i;
-    }
-    do {
-        for (int i = 0; i < N; i++) {
-            cout << a[i] << " ";
-        }
-        cout << "\n";
-    } while (next_permutation(a, a+N));
-    return 0;
-}
